74cf8dad7053a5c0cfdc99a2185a3afb3a1be5c4
JhangYang <jhang.task@gmail.com>
2023-07-18 20:04:53 +0800

    update livewire.js.

--- js/livewire.js
+++ js/livewire.js
@@ -1,10 +1,8 @@
-"use strict";
-
-document.addEventListener('livewire:load', function () {
-    Livewire.on('closeModal', function (modals) {
-        modals.forEach(function (modal) {
-            const modalElement = document.getElementById(modal);
-            console.log(modalElement);
+document.addEventListener('livewire:load', function() {
+    Livewire.on('closeModal', function(modals) {
+        Object.keys(modals).forEach(function(modal) {
+            const modalKey = modals[modal];
+            const modalElement = document.getElementById(modalKey);
             const modalInstance = bootstrap.Modal.getInstance(modalElement);
             if (modalInstance) {
                 modalInstance.hide();
